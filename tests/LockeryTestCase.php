fa83ea97c6e347bf8602937fddc461f328e9e1f5
CurtisBarogla <curtis_barogla@outlook.fr>
2018-10-06 20:06:13 +0200

    Base class for all tests

---
+++ tests/LockeryTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Lockery component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+ 
+namespace NessTest\Component\Lockery;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Common to all tests for Lockery component
+ *
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+abstract class LockeryTestCase extends TestCase
+{
+    
+}
