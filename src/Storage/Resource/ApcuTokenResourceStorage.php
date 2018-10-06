de7359eacffa1ce6696bf95502c1b568f822e15e
CurtisBarogla <curtis_barogla@outlook.fr>
2018-10-06 20:04:28 +0200

    ResourceTokenStore declaration + implementation

---
+++ src/Storage/Resource/ApcuTokenResourceStorage.php
@@ -0,0 +1,68 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Lockery component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+ 
+namespace Ness\Component\Lockery\Storage\Resource;
+
+/**
+ * Use apcu as a token resource store
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+class ApcuTokenResourceStorage implements TokenResourceStorageInterface
+{
+    
+    /**
+     * Identify keys stored via this storage
+     * 
+     * @var string
+     */
+    public const KEY_PREFIX = "ness_apcu_resource_token_";
+    
+    /**
+     * {@inheritDoc}
+     * @see \Ness\Component\Lockery\Storage\Resource\TokenResourceStorageInterface::get()
+     */
+    public function get(string $resource): ?string
+    {
+        return (false !== $token = \apcu_fetch(self::KEY_PREFIX.$resource)) ? $token : null;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Ness\Component\Lockery\Storage\Resource\TokenResourceStorageInterface::add()
+     */
+    public function add(string $resource, string $token, int $validitity): bool
+    {
+        return \apc_store(self::KEY_PREFIX.$resource, $token, $validitity);
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Ness\Component\Lockery\Storage\Resource\TokenResourceStorageInterface::remove()
+     */
+    public function remove(string $resource): bool
+    {
+        return \apcu_delete(self::KEY_PREFIX.$resource);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Ness\Component\Lockery\Storage\Resource\TokenResourceStorageInterface::clear()
+     */
+    public function clear(): void
+    {
+        foreach (new \APCuIterator("#" . self::KEY_PREFIX . "#", APC_ITER_KEY) as $key => $value)   
+            \apcu_delete($key);
+    }
+
+}
