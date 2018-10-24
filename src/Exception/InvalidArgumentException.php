38b6c7778a46bb9caf5102d4b8ef556335f6bffc
CurtisBarogla <curtis_barogla@outlook.fr>
2018-10-24 15:12:15 +0200

    Exception

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,24 @@
+<?php
+//StrictType
+declare(strict_types = 1);
+
+/*
+ * Ness
+ * Lockey component
+ *
+ * Author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+ 
+namespace Ness\Component\Lockey\Exception;
+
+/**
+ * When invalid argument is given
+ * 
+ * @author CurtisBarogla <curtis_barogla@outlook.fr>
+ *
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+    //
+}
